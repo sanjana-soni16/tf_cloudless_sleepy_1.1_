607e6b0d9aa3d028b6e0db0233ffa36b04063fff
Amy <33025632+theamanraj@users.noreply.github.com>
2020-06-25 13:43:52 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,6 +5,11 @@ description = "A x to pass to the template."
 default = "hello"
 }
 
+variable "z" {
+description = "How long our local-exec will not take a nap."
+default = 0
+}
+
 variable "y" {
 description = "A y to pass to the template."
 default = "hello"
@@ -15,7 +20,4 @@ description = "How long our local-exec will take a nap."
 default = 0
 }
 
-variable "z" {
-description = "How long our local-exec will not take a nap."
-default = 0
-}
+
