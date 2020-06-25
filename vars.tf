f1bc40159ee86be19f6f94bcc4149609b9f12f06
Amy <33025632+theamanraj@users.noreply.github.com>
2020-06-25 12:50:11 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,11 +1,21 @@
 # Configure these variables
 
-variable "sample_var" {
-  description = "A sample_var to pass to the template."
-  default     = "hello"
+variable "x" {
+description = "A x to pass to the template."
+default = "hello"
+}
+
+variable "y" {
+description = "A y to pass to the template."
+default = "hello"
 }
 
 variable "sleepy_time" {
-  description = "How long our local-exec will take a nap."
-  default     = 0
+description = "How long our local-exec will take a nap."
+default = 0
+}
+
+variable "z" {
+description = "How long our local-exec will not take a nap."
+default = 0
 }
