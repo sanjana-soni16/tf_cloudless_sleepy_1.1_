588dbd23fc879c76f5ae4466c5a8eba8244e70ac
Amy <33025632+theamanraj@users.noreply.github.com>
2020-08-13 19:20:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 # Create an arbitrary local resource
+provider "ibm" {  
+  #ibmcloud_api_key = var.ibmcloud_api_key
+  generation = local.generation
+  version    = "~> 1.5.3"
+}
+
 data "template_file" "test" {
   template = "Hello, I am a template. My sample_var value = $${sample_var}"
 
