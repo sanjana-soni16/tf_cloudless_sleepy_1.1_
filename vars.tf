394ea75e365c9ddc4515d2c64e6d9bb63db1d158
Amy <33025632+theamanraj@users.noreply.github.com>
2020-08-20 09:54:09 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -19,5 +19,8 @@ variable "sleepy_time" {
 description = "How long our local-exec will take a nap."
 default = 0
 }
-variable "ibmcloud_api_key" {}
+variable "ibmcloud_api_key" {
+description = "How ibmcloud_api_key will take a nap."
+default = "abc"
+}
 
